f683b98a9a7fa734b52dfaff47c4f161b4519258
NicolasGallina01 <nicolas.gallina@aluno.ifsp.edu.br>
2025-10-12 18:24:04 -0300

    interface de usuário

---
+++ interface_sec.gd
@@ -0,0 +1,18 @@
+extends VBoxContainer
+
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/Mundo.tscn")
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/Mundo.tscn")
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/Mundo.tscn")
+
+func _on_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/Mundo.tscn")
+
+func _on_button_5_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/interface_ini.tscn")
