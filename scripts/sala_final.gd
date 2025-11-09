930c1f6718e4908709e2de22a5e6e9b13b6734c5
FaellEmiliano <rafaemiliano2014@gmail.com>
2025-11-08 23:19:44 -0300

    barra de vida consertada

--- scripts/sala_final.gd
+++ scripts/sala_final.gd
@@ -1,13 +1,15 @@
 extends Node2D
 var player_entrou :bool = false
-var inimigos :int = 2
+var inimigos :int = 0
 @onready var tilemap = $TSala
 var vizinhos_instanciados :Array
 var pesos_inimigos = [2,4,4]
+var boss = load("res://cenas/zumbi_boss2.tscn")
+var boss2 = load("res://cenas/esqueleto_boss2.tscn")
 
 
 func _on_area_sala_body_exited(body: Node2D) -> void:
-	if body.is_in_group("enimy"):
+	if body.is_in_group("enemy"):
 		inimigos -= 1
 		if inimigos <=0:
 			abrir_portas(vizinhos_instanciados)
@@ -41,10 +43,17 @@ func _on_area_sala_body_entered(body: Node2D) -> void:
 	if body.is_in_group("character") and not player_entrou:
 		eventos.desenhar_mapa.emit(self)
 		fechar_portas(vizinhos_instanciados)
-		
+		var zumbi = boss.instantiate()
+		zumbi.position = Vector2(480,286)
+		add_child(zumbi)
+		inimigos += 1
+		var esque = boss2.instantiate()
+		esque.position = Vector2(480,286)
+		add_child(esque)
+		inimigos += 1
 	
 
 
 func _on_saida_body_entered(body: Node2D) -> void:
 	if inimigos <= 0:
-		get_tree().change_scene_to_file("res://cenas/interface_sec.tscn")
+		get_tree().change_scene_to_file("res://cenas/creditos.tscn")
